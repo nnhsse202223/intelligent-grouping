ecefc5e68ad0cb150bc43aac2a514ba425fa03d1
Patrick <89258705+PatrickBaldinger@users.noreply.github.com>
2023-02-24 10:56:47 -0600

    Finished documenting script.js at a higher level

--- static/script.js
+++ static/script.js
@@ -21,11 +21,18 @@ let state = {mode: 0, info: {}}
 
 startLoad()
 
+/*
+ * @param {number} mode - The mode to set the app to
+ * @param {object} info - Any additional information to pass to the app
+ */
 function setState(mode, info={}) {
   state.mode = mode
   state.info = info
 }
 
+/*
+ * @description - resets the entire app
+ */
 function resetApp() {
   username.innerText = ""
   clearDiv(classListDiv)
