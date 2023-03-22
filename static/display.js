e1b7504903d1a30a1ebc8e4e771df641e1fcdf90
bfkp2006 <bfkildenpedersen@stu.naperville203.org>
2023-03-22 11:56:58 -0500

    groupScatter is fitted correctly in displayMode edited fullscreen class

--- static/display.js
+++ static/display.js
@@ -2,5 +2,6 @@ openDisplayMode.addEventListener("click", () => {
     // mainSidebar.style.display = "none";
     // showFade(displayGroupScatter)
     switchMultipleSections(displayMode, groupScatter)
-    fullscreen(groupScatter)
+    showFullscreen(groupScatter)
+    // showFullscreen()
 })
--- static/ui.js
+++ static/ui.js
@@ -64,6 +64,10 @@ function hideFullscreen(element) {
   element.classList.remove("fullscreen")
 }
 
+// function top(element) {
+//   element.classList.add("top")
+// }
+
 function switchSection(to) {
   const sections = Array.from(document.getElementsByClassName("section"))
   for (const section of sections) {
@@ -83,6 +87,7 @@ function switchMultipleSections(sec1, sec2 ) {
   }
   showFade(sec1)
   showFade(sec2)
+  // top(sec2)
 }
 
 function removeList(element) {
