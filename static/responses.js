d82d393427ca17d5ff2db0afae8874923c7eb8fe
lwgolobitsh <lwgolobitsh@stu.naperville203.org>
2023-03-21 22:24:01 -0500

    Implemented code to automatically refresh the "view preferences" screen

--- static/responses.js
+++ static/responses.js
@@ -1,3 +1,5 @@
+let currentStudent;
+
 function showResponses() {
     statusTitle.innerText = "Student Responses"
     switchSection(responsesSection)
@@ -32,7 +34,9 @@ function appendClassStudents(){
     newOption.setAttribute('id', classes[state.info.id].obj.students[i].id)
     newOption.setAttribute('index', i)
     newOption.addEventListener('click', function(){
-      updateStudentInformation(newOption.getAttribute('index'));
+      // updateStudentInformation(newOption.getAttribute('index'));
+      currentStudent = newOption.getAttribute('index');
+      reloadPreferencesDisplay(currentStudent);
     });
     newOption.innerText = classes[state.info.id].obj.students[i].first + " " + classes[state.info.id].obj.students[i].last
 
@@ -124,7 +128,34 @@ function findStudentById(id){
   }
 }
 
-reloadResponses.addEventListener("click", async (e) =>{
+// reloadResponses.addEventListener("click", async (e) =>{
+//   //e.stopPropagation()
+//   let databaseClasses = await fetch("/getClasses", {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//       token: auth2.currentUser.get().getAuthResponse().id_token
+//     }}).then(response => response.json())//.then((json)=> {console.log(json.classes)})
+//     // console.log(classes)
+//     // console.log(databaseClasses)
+
+//     //Things surrounded by // and \\ are important notes
+//     //classes structure (classes is an object where every key is a class id and the value is an object with the class object and an array of student ids)
+//     //classes = {classId: {obj: classObj, element: //this is an important div element for the button for the specific class on the left panel we need to find a way to not lose this when recreating the classes array\\}
+    
+//     //json structure from database (json is an array of class objects)
+//     //[{groupings:groupings, id:classId //<-very important... is the same as the object key in the classes array\\, name:name, period:period, students:students, _id:_id}]
+
+//     //loops through the classes array from the GET requst (json)
+//     for(let i = 0; i < databaseClasses.classes.length; i++) {
+//       //recreates the classes array from the GET request restoring the old element and new class object
+//       classes[databaseClasses.classes[i].id] = {element:classes[databaseClasses.classes[i].id].element, obj:databaseClasses.classes[i]}
+//       // console.log(databaseClasses[i].id)
+//     }
+// })
+
+async function reloadPreferencesDisplay(index) {
+  console.log("reloading classes")
   //e.stopPropagation()
   let databaseClasses = await fetch("/getClasses", {
     method: "GET",
@@ -148,12 +179,17 @@ reloadResponses.addEventListener("click", async (e) =>{
       classes[databaseClasses.classes[i].id] = {element:classes[databaseClasses.classes[i].id].element, obj:databaseClasses.classes[i]}
       // console.log(databaseClasses[i].id)
     }
-})
+
+    updateStudentInformation(index)
+}
+
+// reloads classes every 10 seconds 
+setInterval(() => {reloadPreferencesDisplay(currentStudent)}, 10000)
 
 /*
 SOULTION TO THE REFRESH PROBLEM \'o'/
 
-setTimeout(functionName,howeverManyMillisecondsBetweenFunctionCalls)
+setInterval(functionName,howeverManyMillisecondsBetweenFunctionCalls)
 
 funtionName - function that gets form data and puts it into the database and UI
                     Might need to make a new function for this
