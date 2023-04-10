20f2fd5f572d963dfc47eefd50425d84f853dc38
Luke Golobitsh <73967796+lwgolobitsh@users.noreply.github.com>
2023-04-10 10:58:55 -0500

    Removed add group button from displaymode

--- static/display.js
+++ static/display.js
@@ -2,6 +2,7 @@ openDisplayMode.addEventListener("click", () => {
     // switchMultipleSections(displayMode, groupScatter)
     // showFullscreen(groupScatter)
     fullGroupScatter.innerHTML = groupScatter.innerHTML;
+    fullGroupScatter.querySelector("#add-group").remove()
     showFullscreen(fullGroupScatter)
     switchSection(displayMode)
 })
